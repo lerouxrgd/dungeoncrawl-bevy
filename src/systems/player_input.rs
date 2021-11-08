85500999b3a03b4d2193d753ca8adb3550ced61d
Romain Leroux <romain@leroux.dev>
2021-11-08 22:17:41 +0100

    Add healing system

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -5,7 +5,7 @@ pub fn player_input(
     mut turn_state: ResMut<State<TurnState>>,
     mut ev_movements: EventWriter<WantsToMove>,
     mut ev_attacks: EventWriter<WantsToAttack>,
-    mut player_query: Query<(Entity, &Point), With<Player>>,
+    mut player_query: Query<(Entity, &Point, &mut Health), With<Player>>,
     enemies_query: Query<(Entity, &Point), With<Enemy>>,
 ) {
     for ev in key_evr.iter().take(1) {
@@ -14,30 +14,39 @@ pub fn player_input(
             (ElementState::Pressed, Some(KeyCode::Left)) => Point::new(-1, 0),
             (ElementState::Pressed, Some(KeyCode::Down)) => Point::new(0, -1),
             (ElementState::Pressed, Some(KeyCode::Right)) => Point::new(1, 0),
+            (ElementState::Pressed, Some(_)) => Point::new(0, 0),
             _ => return,
         };
 
-        let (player, &origin) = player_query.single_mut().unwrap();
-        let destination = origin + delta;
+        let (player, &origin, mut health) = player_query.single_mut().unwrap();
 
-        let mut hit_something = false;
-        enemies_query
-            .iter()
-            .filter(|(_, &pos)| pos == destination)
-            .for_each(|(enemy, _)| {
-                hit_something = true;
+        // No movement => heal
+        if delta == Point::new(0, 0) {
+            health.current = i32::min(health.max, health.current + 1);
+        } else
+        // Move or attack
+        {
+            let destination = origin + delta;
 
-                ev_attacks.send(WantsToAttack {
-                    attacker: player,
-                    victim: enemy,
+            let mut hit_something = false;
+            enemies_query
+                .iter()
+                .filter(|(_, &pos)| pos == destination)
+                .for_each(|(enemy, _)| {
+                    hit_something = true;
+
+                    ev_attacks.send(WantsToAttack {
+                        attacker: player,
+                        victim: enemy,
+                    });
                 });
-            });
 
-        if !hit_something {
-            ev_movements.send(WantsToMove {
-                entity: player,
-                destination,
-            });
+            if !hit_something {
+                ev_movements.send(WantsToMove {
+                    entity: player,
+                    destination,
+                });
+            }
         }
 
         turn_state.set(TurnState::PlayerTurn).unwrap();
